109d3afab2da74df57d34016885fe57726de0e64
Olgerd425 <78820918+Olgerd425@users.noreply.github.com>
2021-02-17 20:11:37 +0200

    task-15

---
+++ task-15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число 1:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число 2:");
+            var b = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                Console.Write($"{a}<{b}");
+
+            }
+            else if (a>b) {
+                Console.Write($"{a}>{b}");
+            }
+            else {
+                Console.Write($"{a}={b}");
+            }
+        }
+    }
+}
